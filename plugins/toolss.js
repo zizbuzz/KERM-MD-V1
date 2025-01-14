e1b7e4403b7f52fcea1ba55ccfd8e474af08f1b2
KermRayan <kermd237@gmail.com>
2025-01-14 21:10:41 +0100

    Update toolss.js

--- plugins/toolss.js
+++ plugins/toolss.js
@@ -1,10 +1,15 @@
+/*
+_  ______   _____ _____ _____ _   _
+| |/ / ___| |_   _| ____/___ | | | |
+| ' / |  _    | | |  _|| |   | |_| |
+| . \ |_| |   | | | |__| |___|  _  |
+|_|\_\____|   |_| |_____\____|_| |_|
 
-
-
-
-
-
-
+ANYWAY, YOU MUST GIVE CREDIT TO MY CODE WHEN COPY IT
+CONTACT ME HERE +237656520674
+YT: KermHackTools
+Github: Kgtech-cmr
+*/
 
 
 const fs = require('fs');
