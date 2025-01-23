e5e0227b8dabf6813c1ac6196b2b758e9d28c46b
KermRayan <kermd237@gmail.com>
2025-01-23 11:02:07 +0100

    Update translator.js

--- plugins/translator.js
+++ plugins/translator.js
@@ -1,11 +1,15 @@
-/*created by Kgtech 🕵
-contact dev1 237656520674 ♻️
-contact dev2 237650564445 ♻️
-© Copy coder alert ⚠
+/*
+_  ______   _____ _____ _____ _   _
+| |/ / ___| |_   _| ____/___ | | | |
+| ' / |  _    | | |  _|| |   | |_| |
+| . \ |_| |   | | | |__| |___|  _  |
+|_|\_\____|   |_| |_____\____|_| |_|
+
+ANYWAY, YOU MUST GIVE CREDIT TO MY CODE WHEN COPY IT
+CONTACT ME HERE +237656520674
+YT: KermHackTools
+Github: Kgtech-cmr
 */
-
-
-
 
 const axios = require('axios');
 const config = require('../config')
@@ -34,7 +38,7 @@ async (conn, mek, m, { from, q, reply }) => {
         const translation = response.data.responseData.translatedText;
 
         const translationMessage = `
-🌍 *KERM_MD-V4 TRANSLATION* 🌍
+🌍 *KERM-MD-V1 TRANSLATION* 🌍
 
 🔤 *Original*: ${textToTranslate}
 
