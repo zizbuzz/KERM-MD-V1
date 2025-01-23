6a9b1d69e4f56572033dc8d13e7c6a5d1a83c4fd
KermRayan <kermd237@gmail.com>
2025-01-23 10:32:18 +0100

    Update logo.js

--- plugins/logo.js
+++ plugins/logo.js
@@ -22,7 +22,7 @@ const {
 cmd({
   'pattern': "logo",
   'desc': "Create logos",
-  'react': '🎁',
+  'react': '🎨',
   'category': "other",
   'filename': __filename
 }, async (_0x218f5d, _0x28282a, _0x208f73, {
