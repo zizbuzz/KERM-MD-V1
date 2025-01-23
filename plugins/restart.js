8fca0cdc6695659cdf65b9db2faca648700f8e8b
KermRayan <kermd237@gmail.com>
2025-01-23 10:55:00 +0100

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -1,10 +1,15 @@
-/*created by Kgtech 🕵
-contact dev1 237656520674 ♻️
-contact dev2 237650564445 ♻️
-© Copy coder alert ⚠
+/*
+_  ______   _____ _____ _____ _   _
+| |/ / ___| |_   _| ____/___ | | | |
+| ' / |  _    | | |  _|| |   | |_| |
+| . \ |_| |   | | | |__| |___|  _  |
+|_|\_\____|   |_| |_____\____|_| |_|
+
+ANYWAY, YOU MUST GIVE CREDIT TO MY CODE WHEN COPY IT
+CONTACT ME HERE +237656520674
+YT: KermHackTools
+Github: Kgtech-cmr
 */
-
-
 
 const config = require('../config')
 const {cmd , commands} = require('../command')
