57bb3712fd266607070eab388c50435176fe7ae5
KermRayan <kermd237@gmail.com>
2025-01-23 10:54:39 +0100

    Update repoinfo.js

--- plugins/repoinfo.js
+++ plugins/repoinfo.js
@@ -1,11 +1,15 @@
-/*created by Kgtech 🕵
-contact dev1 237656520674 ♻️
-contact dev2 237650564445 ♻️
-© Copy coder alert ⚠
+/*
+_  ______   _____ _____ _____ _   _
+| |/ / ___| |_   _| ____/___ | | | |
+| ' / |  _    | | |  _|| |   | |_| |
+| . \ |_| |   | | | |__| |___|  _  |
+|_|\_\____|   |_| |_____\____|_| |_|
+
+ANYWAY, YOU MUST GIVE CREDIT TO MY CODE WHEN COPY IT
+CONTACT ME HERE +237656520674
+YT: KermHackTools
+Github: Kgtech-cmr
 */
-
-
-
 
 const axios = require('axios');
 const { cmd, commands } = require('../command');
@@ -28,7 +32,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        let repoInfo = `📁_*GITHUB REPOSITORY INFO BY KERM_MD-V4*_📁\n\n`;
+        let repoInfo = `📁_*GITHUB REPOSITORY INFO BY KERM-MD-V1*_📁\n\n`;
         repoInfo += `📌 *ɴᴀᴍᴇ*: ${data.name}\n`;
         repoInfo += `🔗 *ᴜʀʟ*: ${data.html_url}\n`;
         repoInfo += `📝 *ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ*: ${data.description}\n`;
