c80aac99b80a2dbb2b2fe11547ae94c1fc547ea9
KermRayan <kermd237@gmail.com>
2025-01-26 18:12:08 +0100

    Update save.js

--- plugins/save.js
+++ plugins/save.js
@@ -1,3 +1,18 @@
+/*
+_  ______   _____ _____ _____ _   _
+| |/ / ___| |_   _| ____/___ | | | |
+| ' / |  _    | | |  _|| |   | |_| |
+| . \ |_| |   | | | |__| |___|  _  |
+|_|\_\____|   |_| |_____\____|_| |_|
+
+ANYWAY, YOU MUST GIVE CREDIT TO MY CODE WHEN COPY IT
+CONTACT ME HERE +237656520674
+YT: KermHackTools
+Github: Kgtech-cmr
+*/
+
+
+
 const {
   cmd,
   commands
