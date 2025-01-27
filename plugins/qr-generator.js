328f7e06666cbe62c3c406f18fc9b3813229bd62
KermRayan <kermd237@gmail.com>
2025-01-27 12:54:20 +0100

    Update qr-generator.js

--- plugins/qr-generator.js
+++ plugins/qr-generator.js
@@ -48,7 +48,7 @@ cmd({
 }) => {
   try {
     if (!q) return reply('Please provide text to generate QR code.');
-    await reply('> *Kerm Generating QR code...🔄*');
+    await reply('> *Kerm Generating QR code...*');
     const apiUrl = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(q)}&size=200x200`;
     const response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
     const buffer = Buffer.from(response.data, 'binary');
