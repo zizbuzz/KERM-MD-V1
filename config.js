0b7ece1108360ce5ba5401c110f46165f65f57ca
KermRayan <kermd237@gmail.com>
2025-01-29 09:43:41 +0100

    Update config.js

--- config.js
+++ config.js
@@ -19,29 +19,24 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
-    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-    MODE: process.env.MODE || "private",
-    AUTO_VOICE: process.env.AUTO_VOICE || "false",
-    AUTO_STICKER: process.env.AUTO_STICKER || "false",
-    AUTO_REPLY: process.env.AUTO_REPLY || "false",
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/yw2ty2.jpeg",
-    ALIVE_MSG: process.env.ALIVE_MSG || "_Hi 💁🏽 How Can I Assist You. Am alive Now._",
-    ANTI_LINK: process.env.ANTI_LINK || "true",
-    ANTI_BAD: process.env.ANTI_BAD || "true",
-    PREFIX: process.env.PREFIX || ".",
-    FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
-    FAKE_TYPING: process.env.FAKE_TYPING || "false",
-    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
-    CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
-    AUTO_REACT: process.env.AUTO_REACT || "false",
-    HEART_REACT: process.env.HEART_REACT || "false",
-    OWNER_REACT: process.env.OWNER_REACT || "false",
-    BOT_NAME: process.env.BOT_NAME || "『𝗞𝗘𝗥𝗠 𝗠𝗗 𝗩1』",
-    OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
-    // Configuration pour LIKE STATUS
-    likeStatus: {
-        enabled: process.env.LIKE_STATUS_ENABLED || "true", // Mettre à false pour désactiver par défaut
-        defaultEmoji: process.env.LIKE_STATUS_EMOJI || '👍', // Emoji par défaut pour liker les statuts
-    },
+SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "private",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/yw2ty2.jpeg",
+ALIVE_MSG: process.env.ALIVE_MSG || "_Hi 💁🏽 How Can I Assist You. Am alive Now._",
+ANTI_LINK: process.env.ANTI_LINK || "true",
+ANTI_BAD: process.env.ANTI_BAD || "true",
+PREFIX: process.env.PREFIX || ".",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
+FAKE_TYPING: process.env.FAKE_TYPING || "false",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
+CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
+AUTO_REACT: process.env.AUTO_REACT || "false",
+HEART_REACT: process.env.HEART_REACT || "false",
+OWNER_REACT: process.env.OWNER_REACT || "false",
+BOT_NAME: process.env.BOT_NAME || "『𝗞𝗘𝗥𝗠 𝗠𝗗 𝗩1』",
+OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
