c7032de301b9529f690f9e9b5a23367eac8ccd8c
KermRayan <kermd237@gmail.com>
2025-02-08 01:39:34 +0100

    Update anti-view-once.js

--- plugins/anti-view-once.js
+++ plugins/anti-view-once.js
@@ -74,51 +74,100 @@ async (conn, mek, m, { from, reply }) => {
 });
 */
 cmd({
-    pattern: "vv",
-    react : "👀",
-    alias: ['retrive', "viewonce"],
-    desc: "Fetch and resend a ViewOnce message content (image/video/voice).",
-    category: "misc",
-    use: '<query>',
-    filename: __filename
-},
-async (conn, mek, m, { from, reply }) => {
-    try {
-        const quotedMessage = m.msg.contextInfo.quotedMessage; // Get quoted message
-        
-        if (!quotedMessage) {
-            return reply("Please reply to a ViewOnce message.");
-        }
-
-        const viewOnceMessage = quotedMessage.viewOnceMessageV2 || quotedMessage.viewOnceMessage;
-        
-        if (!viewOnceMessage) {
-            return reply("> *This is not a ViewOnce message.*");
-        }
-
-        // Fetch the media message and send it back
-        if (viewOnceMessage.message.imageMessage) {
-            let cap = viewOnceMessage.message.imageMessage.caption || '';
-            let mediaPath = await conn.downloadAndSaveMediaMessage(viewOnceMessage.message.imageMessage);
-            return conn.sendMessage(from, { image: { url: mediaPath }, caption: cap }, { quoted: mek });
-        }
-
-        if (viewOnceMessage.message.videoMessage) {
-            let cap = viewOnceMessage.message.videoMessage.caption || '';
-            let mediaPath = await conn.downloadAndSaveMediaMessage(viewOnceMessage.message.videoMessage);
-            return conn.sendMessage(from, { video: { url: mediaPath }, caption: cap }, { quoted: mek });
-        }
-
-        if (viewOnceMessage.message.audioMessage) {
-            let mediaPath = await conn.downloadAndSaveMediaMessage(viewOnceMessage.message.audioMessage);
-            return conn.sendMessage(from, { audio: { url: mediaPath } }, { quoted: mek });
-        }
-
-        // If the message type is not supported
-        return reply("> *This type of ViewOnce message is not supported.*");
-        
-    } catch (e) {
-        console.error("Error fetching ViewOnce message:", e);
-        reply("An error occurred while fetching the ViewOnce message.");
+  'pattern': 'vv',
+  'alias': ['retrive', 'viewonce'],
+  'desc': "Fetch and resend a ViewOnce message content (image/video/voice).",
+  'category': 'misc',
+  'use': "<query>",
+  'filename': __filename
+}, async (_0x5a104f, _0x1d2e0c, _0x1759df, {
+  from: _0x4d7a74,
+  reply: _0x5545e9
+}) => {
+  try {
+    const _0x1c9e5c = _0x1759df.msg.contextInfo.quotedMessage;
+    if (_0x1c9e5c && _0x1c9e5c.viewOnceMessageV2) {
+      const _0x3798cf = _0x1c9e5c.viewOnceMessageV2;
+      if (_0x3798cf.message.imageMessage) {
+        let _0x18b540 = _0x3798cf.message.imageMessage.caption;
+        let _0x345d25 = await _0x5a104f.downloadAndSaveMediaMessage(_0x3798cf.message.imageMessage);
+        return _0x5a104f.sendMessage(_0x4d7a74, {
+          'image': {
+            'url': _0x345d25
+          },
+          'caption': _0x18b540
+        }, {
+          'quoted': _0x1d2e0c
+        });
+      }
+      if (_0x3798cf.message.videoMessage) {
+        let _0x4fb901 = _0x3798cf.message.videoMessage.caption;
+        let _0x2e752e = await _0x5a104f.downloadAndSaveMediaMessage(_0x3798cf.message.videoMessage);
+        return _0x5a104f.sendMessage(_0x4d7a74, {
+          'video': {
+            'url': _0x2e752e
+          },
+          'caption': _0x4fb901
+        }, {
+          'quoted': _0x1d2e0c
+        });
+      }
+      if (_0x3798cf.message.audioMessage) {
+        let _0x137019 = await _0x5a104f.downloadAndSaveMediaMessage(_0x3798cf.message.audioMessage);
+        return _0x5a104f.sendMessage(_0x4d7a74, {
+          'audio': {
+            'url': _0x137019
+          }
+        }, {
+          'quoted': _0x1d2e0c
+        });
+      }
     }
+    if (!_0x1759df.quoted) {
+      return _0x5545e9("Please reply to a ViewOnce message.");
+    }
+    if (_0x1759df.quoted.mtype === "viewOnceMessage") {
+      if (_0x1759df.quoted.message.imageMessage) {
+        let _0x4e23b1 = _0x1759df.quoted.message.imageMessage.caption;
+        let _0x14d7c9 = await _0x5a104f.downloadAndSaveMediaMessage(_0x1759df.quoted.message.imageMessage);
+        return _0x5a104f.sendMessage(_0x4d7a74, {
+          'image': {
+            'url': _0x14d7c9
+          },
+          'caption': _0x4e23b1
+        }, {
+          'quoted': _0x1d2e0c
+        });
+      } else {
+        if (_0x1759df.quoted.message.videoMessage) {
+          let _0x4cf2b0 = _0x1759df.quoted.message.videoMessage.caption;
+          let _0x5655fd = await _0x5a104f.downloadAndSaveMediaMessage(_0x1759df.quoted.message.videoMessage);
+          return _0x5a104f.sendMessage(_0x4d7a74, {
+            'video': {
+              'url': _0x5655fd
+            },
+            'caption': _0x4cf2b0
+          }, {
+            'quoted': _0x1d2e0c
+          });
+        }
+      }
+    } else {
+      if (_0x1759df.quoted.message.audioMessage) {
+        let _0x5dfa37 = await _0x5a104f.downloadAndSaveMediaMessage(_0x1759df.quoted.message.audioMessage);
+        return _0x5a104f.sendMessage(_0x4d7a74, {
+          'audio': {
+            'url': _0x5dfa37
+          }
+        }, {
+          'quoted': _0x1d2e0c
+        });
+      } else {
+        return _0x5545e9("> *This is not a ViewOnce message.*");
+      }
+    }
+  } catch (_0x23e307) {
+    console.log('Error:', _0x23e307);
+    _0x5545e9("An error occurred while fetching the ViewOnce message.");
+  }
 });
