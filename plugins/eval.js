a5955a88a93fdf31f54c41e6a306d8b98980ab6e
KermRayan <kermd237@gmail.com>
2025-02-24 19:37:36 +0100

    Create eval.js

---
+++ plugins/eval.js
@@ -0,0 +1,23 @@
+
+
+const util = require('util')
+Xcmd(
+  {
+    Xname: "eval",
+    Xclass: "system",
+    Xinfo: "Evaluate code",
+    Xpath: __filename,
+  },
+  async (m, match) => {
+    let c = match;
+    if (!c) return m.reply(`_Please provide code to run_`);
+
+    try {
+      let s = await eval(c);
+      if (typeof s !== "string") s = util.inspect(s);
+      await m.reply(s);
+    } catch (e) {
+      await m.reply(util.format(e));
+    }
+  }
+);
