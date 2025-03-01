5569e334a1bc1463141107246414a64270850aaf
zizbuzz <142731617+zizbuzz@users.noreply.github.com>
2025-03-01 05:09:40 +0100

    config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
+SESSION_ID: process.env.SESSION_ID || "PJIiUb5B#N6kBdSkvdTnXicrBf07ZuwQA3LB1CpFF0SxOGIsXYYg",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
